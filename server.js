e20b3e995180d0b847a90e626e1f8edee6b5119a
Eric Moolenbeek <emoolenbeek@gmail.com>
2022-01-10 22:01:04 +0000

    add js files for login and register routes

--- server.js
+++ server.js
@@ -52,12 +52,12 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-
 app.get("/login", (req, res) => {
   res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
 app.listen(PORT, () => {
