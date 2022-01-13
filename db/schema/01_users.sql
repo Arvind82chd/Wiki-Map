48d64dd5aecc7fd5dcff24af526283cd3dc85cbf
Arvind Sharma <mail.arvind.s@gmail.com>
2022-01-13 22:48:22 +0000

    updated schema for pin and refactored the editPin function

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -30,9 +30,10 @@ CREATE TABLE favorite_maps (
 CREATE TABLE pin (
   id SERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  image_url VARCHAR NOT NULL,
   pin_user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   favorite_maps_id INTEGER REFERENCES favorite_maps(id) ON DELETE CASCADE,
-  description TEXT NOT NULL,
   latitude INTEGER NOT NULL,
   longitude INTEGER NOT NULL,
   active BOOLEAN NOT NULL DEFAULT TRUE);
