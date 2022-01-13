85d22d6e42706af02a898da9f481b3763b8413e9
Arvind Sharma <mail.arvind.s@gmail.com>
2022-01-13 19:00:07 +0000

    fixed ERD / schema bug & updated the db.js file by adding pool

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,3 +1,5 @@
+
+
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS map CASCADE;
 DROP TABLE IF EXISTS favorite_maps CASCADE;
@@ -10,28 +12,30 @@ CREATE TABLE users (
   password VARCHAR(255) NOT NULL);
 
 
--- CREATE TABLE map (
---   id SERIAL PRIMARY KEY NOT NULL,
---   title VARCHAR(255) NOT NULL,
---   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
---   pin_id INTEGER REFERENCES pin(id) ON DELETE CASCADE,
---   latitude INTEGER NOT NULL,
---   longitude INTEGER NOT NULL);
+CREATE TABLE map (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  latitude INTEGER NOT NULL,
+  longitude INTEGER NOT NULL);
 
 
 
--- CREATE TABLE favorite_maps (
---   id SERIAL PRIMARY KEY NOT NULL,
---   map_user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
---   map_id INTEGER REFERENCES map(id) ON DELETE CASCADE);
+CREATE TABLE favorite_maps (
+  id SERIAL PRIMARY KEY NOT NULL,
+  map_user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  map_id INTEGER REFERENCES map(id) ON DELETE CASCADE);
 
 
--- CREATE TABLE pin (
---   id SERIAL PRIMARY KEY NOT NULL,
---   title VARCHAR(255) NOT NULL,
---   pin_user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
---   favorite_maps_id INTEGER REFERENCES favorite_maps(id) ON DELETE CASCADE,
---   description TEXT NOT NULL,
---   latitude INTEGER NOT NULL,
---   longitude INTEGER NOT NULL,
---   active BOOLEAN NOT NULL DEFAULT TRUE);
+CREATE TABLE pin (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  pin_user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  favorite_maps_id INTEGER REFERENCES favorite_maps(id) ON DELETE CASCADE,
+  description TEXT NOT NULL,
+  latitude INTEGER NOT NULL,
+  longitude INTEGER NOT NULL,
+  active BOOLEAN NOT NULL DEFAULT TRUE);
+
+
+
