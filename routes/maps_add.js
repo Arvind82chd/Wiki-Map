846c0249f3cbeba7902232086cd9136024109bf2
Eric Moolenbeek <emoolenbeek@gmail.com>
2022-01-14 01:31:24 +0000

    add maps_add routes

--- routes/maps_add.js
+++ routes/maps_add.js
@@ -1,8 +1,67 @@
 const express = require('express');
 const router = express.Router();
+const { Pool } = require('pg');
+const pool = new Pool ({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'midterm',
+  port: '5432'
+});
+const getIdFromEmail = email => {
+  const queryString = `SELECT id FROM users WHERE email = $1;`;
+  const values = [email];
+
+  return pool
+  .query(queryString, values)
+  .then(res => res.rows[0].id)
+  .catch(e => console.error(e.stack))
+}
+
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    res.render("maps_add");
+    res.render("maps_add", {
+      user: req.session.email
+    });
   });
+
+
+  // check db if map exists
+  // if exists throw error
+  // if map doesn't exist insert map into db
+
+  router.post("/", (req, res) => {
+    const body = req.body;
+    console.log('body:', body);
+
+    // check db if email exists
+    // if exists throw error
+    // if email doesn't exist hash password and insert email and password into database
+    // create session (cookie)
+
+    // redirect to index
+    db.query(`SELECT * FROM map WHERE title = $1;`, [body.title])
+      .then(async data => {
+        console.log('data.rows:', data.rows)
+
+        if (data.rows.length > 0) {
+          console.log('map exists')
+          res.render("maps_add", {
+            error: 'this map already exists',
+            user: null
+          })
+        }
+        db.query(`INSERT INTO map (
+               title, user_id)
+               VALUES ($1, $2) RETURNING *;`, [body.title, getIdFromEmail(req.session.email)])
+          .then(
+            data => {
+              data.rows[0];
+              res.render("index", {user: req.session.email});
+            }
+          )
+      })
+  });
+
   return router;
 };
