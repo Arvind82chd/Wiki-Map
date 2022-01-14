50c8539994f1757b9adb65d76e986da3f15920ac
Arvind Sharma <mail.arvind.s@gmail.com>
2022-01-14 00:07:41 +0000

    updated register.post and its subsequent modules

--- routes/register.js
+++ routes/register.js
@@ -4,5 +4,19 @@ module.exports = (db) => {
   router.get("/", (req, res) => {
     res.render("register");
   });
+  router.post('/register', (req, res) => {
+    const email_id = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
+    const user_id = (/*user_id to be assigned from db's sequence*/);
+    users[user_id] = {
+      id: user_id,
+      name: name,
+      email: email_id,
+      password: password
+    }
+
+  });
   return router;
+
 };
