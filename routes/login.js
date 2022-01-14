7d4a8b9963b55674ea4fc39cd56386938e851909
Eric Moolenbeek <emoolenbeek@gmail.com>
2022-01-14 14:09:06 +0000

    add async await

--- routes/login.js
+++ routes/login.js
@@ -19,9 +19,9 @@ const getMaps = () => {
 }
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
+  router.get("/", async (req, res) => {
     const user_email = req.session.email; //to check user loged in
-    const maps = getMaps(); //because we want to send maps data to the index.ejs
+    const maps = await getMaps(); //because we want to send maps data to the index.ejs
 
     const templatevars = {user: user_email, maps: maps}
     res.render("login", templatevars);
